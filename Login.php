273709af5475110eb20c89490467f033d23e84b3
SamuelRM25 <124316227+SamuelRM25@users.noreply.github.com>
2023-05-17 21:11:43 -0600

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,25 +1,38 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bienvenido</title>
+</head>
+<body>
+    <h1>Usuario ingresado</h1>
 
-$data = array( 'Accion' => 'L', 'JsonStructure' => '{"Usuario":"11","Contrasenia":"patito246"}' );
+    <?php
+        $data = array( 'Accion' => 'L', 'JsonStructure' => '{"Usuario":"11","Contrasenia":"patito246"}' );
 
-$curl = curl_init();
+        $curl = curl_init();
 
-curl_setopt_array($curl, array(
-CURLOPT_URL => "https://api-bancovivienda20230424203023.azurewebsites.net/api/LogIn",
-CURLOPT_RETURNTRANSFER => true,
-CURLOPT_ENCODING => "",
-CURLOPT_MAXREDIRS => 10,
-CURLOPT_TIMEOUT => 30,
-CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-CURLOPT_CUSTOMREQUEST => "POST",
-CURLOPT_POSTFIELDS => json_encode($data),
-CURLOPT_SSL_VERIFYPEER => false,
-CURLOPT_HTTPHEADER => array(
-"Cache-Control: no-cache",
-"Content-Type: application/json",
-),
-));
-echo $xmls = curl_exec($curl);
-$err = curl_error($curl);
-//$aaaa = xml_to_array($xmls);
-?>
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api-bancovivienda20230424203023.azurewebsites.net/api/LogIn",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => json_encode($data),
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_HTTPHEADER => array(
+                "Cache-Control: no-cache",
+                "Content-Type: application/json",
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        echo $response;
+
+        curl_close($curl);
+    ?>
+</body>
+</html>
